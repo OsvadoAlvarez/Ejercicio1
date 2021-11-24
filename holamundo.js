e93a8174e2405ca1fbd70380351fbdbaa1671929
ozzy <osviac13@hotmail.com>
2021-11-24 08:25:45 -0700

    ejercicio en php

---
+++ holamundo.js
@@ -0,0 +1,13 @@
+ 'use strict'
+
+console.log('Hola mundo desde Node.js esto se vera en la terminal')
+
+console.log(2*5)
+
+//console.log(window)
+
+console.log(global)
+
+setInterval(function(){
+    console.log('hola nodejs')
+},1000)
